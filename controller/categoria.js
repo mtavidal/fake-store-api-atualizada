7e7a4a1f03eb7803fdfe4e82c767b94a1a03f9be
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-06-25 19:20:46 -0300

    Trata letrar maiusculas e minusculas nomes iguais de categoria

--- controller/categoria.js
+++ controller/categoria.js
@@ -38,7 +38,9 @@ module.exports.addCategoria = async (req, res) => {
 			message: 'data is undefined',
 		});
 	} else {
-		const temCategoriaIgual = await Categoria.findOne({ nome: req.body.nome });
+		const nomeCategoria = `^${diacriticSensitiveRegex(req.body.nome)}$`
+		console.log(nomeCategoria);
+		const temCategoriaIgual = await Categoria.findOne({ nome: { $regex: nomeCategoria, $options: 'i' } });
 		if (!temCategoriaIgual) {
 			Categoria.find()
 				.select(['id'])
@@ -91,3 +93,18 @@ module.exports.deleteCategoria = (req, res) => {
 
 	}
 };
+
+
+function diacriticSensitiveRegex(string = '') {
+	return string
+		.replace(/a/g, '[a,á,à,ä,â]')
+		.replace(/A/g, '[A,a,á,à,ä,â]')
+		.replace(/e/g, '[e,é,ë,è]')
+		.replace(/E/g, '[E,e,é,ë,è]')
+		.replace(/i/g, '[i,í,ï,ì]')
+		.replace(/I/g, '[I,i,í,ï,ì]')
+		.replace(/o/g, '[o,ó,ö,ò]')
+		.replace(/O/g, '[O,o,ó,ö,ò]')
+		.replace(/u/g, '[u,ü,ú,ù]')
+		.replace(/U/g, '[U,u,ü,ú,ù]');
+}
